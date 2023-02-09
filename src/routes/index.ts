0e5705b813de2be9952f9ad8cbbecbe4b6986ef7
Alysson93 <alyssonpereiraassuncao@gmail.com>
2023-02-09 10:13:54 -0300

    Refatoração: usar classes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,10 +1,12 @@
 import { Router } from 'express';
 
-import { CidadesController } from '../controllers';
+//import { CidadesController } from '../controllers';
+import CidadesController from '../controllers/CidadesController'
 
 
 const router = Router();
 
-router.post('/cidades', CidadesController.create);
+//router.post('/cidades', CidadesController.create);
+router.post('/cidades', new CidadesController().create);
 
 export default router;
