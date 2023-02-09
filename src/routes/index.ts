54f616bbed74b03e72cb9932cf206b2a54f68c52
Alysson93 <alyssonpereiraassuncao@gmail.com>
2023-02-09 16:24:51 -0300

    Refatoração: controllers com classes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,12 +1,7 @@
 import { Router } from 'express';
-
-//import { CidadesController } from '../controllers';
-import CidadesController from '../controllers/CidadesController'
-
-
 const router = Router();
 
-//router.post('/cidades', CidadesController.create);
+import CidadesController from '../controllers/CidadesController'
 router.post('/cidades', new CidadesController().create);
 
 export default router;
